285c4eb8705ea77fae6eafe00d53414d946a5702
taishi-i <taishi.ikeda.0323@gmail.com>
2020-08-23 19:25:34 +0900

    update v0.0.6

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Taishi Ikeda'
 author = 'Taishi Ikeda'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.5'
+release = '0.0.6'
 
 
 # -- General configuration ---------------------------------------------------
