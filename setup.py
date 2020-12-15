f9baa8166b8729bf40a6a6c838162583db7185fa
taishi-i <taishi.ikeda.0323@gmail.com>
2020-12-15 16:26:19 +0900

    add toiro to setup.py

--- setup.py
+++ setup.py
@@ -35,28 +35,30 @@ extras_requires_tokenizers = [
 
 extras_requires_classifiers = ['torch', 'transformers', 'catalyst']
 
-setup(
-    name='toiro',
-    version='0.0.8',
-    description='A comparison tool of Japanese tokenizers',
-    author='Taishi Ikeda',
-    author_email='taishi.ikeda.0323@gmail.com',
-    long_description_content_type='text/markdown',
-    long_description=_long_description(),
-    keywords='Japanese NLP',
-    url='https://github.com/taishi-i/toiro',
-    download_url='https://github.com/taishi-i/toiro/archive/0.0.8.tar.gz',
-    packages=['toiro.datadownloader', 'toiro.tokenizers', 'toiro.classifiers'],
-    install_requires=[
-        'requests', 'tqdm', 'pandas', 'sklearn', 'py-cpuinfo', 'janome'
-    ],
-    extras_require={
-        'all_tokenizers': extras_requires_tokenizers,
-        'all_classifiers': extras_requires_classifiers,
-        'all': extras_requires_tokenizers + extras_requires_classifiers
-    },
-    classifiers=classifiers,
-    include_package_data=True,
-    python_requires='>=3.6.0',
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'pytest-cov'])
+setup(name='toiro',
+      version='0.0.8',
+      description='A comparison tool of Japanese tokenizers',
+      author='Taishi Ikeda',
+      author_email='taishi.ikeda.0323@gmail.com',
+      long_description_content_type='text/markdown',
+      long_description=_long_description(),
+      keywords='Japanese NLP',
+      url='https://github.com/taishi-i/toiro',
+      download_url='https://github.com/taishi-i/toiro/archive/0.0.8.tar.gz',
+      packages=[
+          'toiro', 'toiro.datadownloader', 'toiro.tokenizers',
+          'toiro.classifiers'
+      ],
+      install_requires=[
+          'requests', 'tqdm', 'pandas', 'sklearn', 'py-cpuinfo', 'janome'
+      ],
+      extras_require={
+          'all_tokenizers': extras_requires_tokenizers,
+          'all_classifiers': extras_requires_classifiers,
+          'all': extras_requires_tokenizers + extras_requires_classifiers
+      },
+      classifiers=classifiers,
+      include_package_data=True,
+      python_requires='>=3.6.0',
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest', 'pytest-cov'])
