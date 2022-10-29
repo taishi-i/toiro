90391e8ea5f96a7066a60fe6cba4e361f659206d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-29 22:44:57 +0000

    Adding tarfile member sanitization to extractall()

--- toiro/datadownloader/preprocess.py
+++ toiro/datadownloader/preprocess.py
@@ -15,7 +15,26 @@ from .downloader_utils import get_resource_dir
 
 def _extract_tarfile(filename, target_dir):
     with tarfile.open(filename, 'r:*') as tar:
-        tar.extractall(target_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, target_dir)
 
 
 def _shuffle_data(is_shuffle, data):
